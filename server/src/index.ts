dbf7b9702a379efc58e6ae84182d546ca691fa88
Daniyal Aamir <daniyalaamir@daniyals-mbp.myfiosgateway.com>
2021-01-05 18:22:37 -0500

    server: add middleware and create POST route to delete listing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import { listings } from './listings';
 
 const app = express();
 const port = 9000;
+
+// middleware
+app.use(express.json());
 
 const one = 1;
 const two = 2;
@@ -11,6 +15,18 @@ app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`));
 
 app.get('/listings', (_req, res) => {
   return res.send(listings);
+})
+
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+
+  for(let i=0; i<listings.length; i++) {
+    if(listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+
+  return res.send('failed to delete listing');
 })
 
 app.listen(port);
