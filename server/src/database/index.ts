c3fe457e3b60d757f3857e48a55acb7fdffadfac
Daniyal Aamir <daamir89@gmail.com>
2021-01-20 11:24:59 -0500

    server: update mongodb password and hide it from source code

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 const user = 'testuser_001';
-const userPassword = 'TaNpCye9sZ59g5F7';
+const userPassword = '................';
 const cluster = 'cluster0.w6dvl';
 
 const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
