968c2e9ab74056e20c8f14ae6617e1e43eabe2ce
Daniyal Aamir <daamir89@gmail.com>
2021-01-21 15:11:07 -0500

    server: create .env file to load environment variables

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,11 +1,7 @@
 import { MongoClient } from 'mongodb';
 import { Database } from '../lib/types';
 
-const user = 'testuser_001';
-const userPassword = '...............';
-const cluster = 'cluster0.w6dvl';
-
-const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, { 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
+require('dotenv').config();
+
 import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './graphql/index';
 import { connectDatabase } from './database/index';
-
-const port = 9000;
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
@@ -15,9 +15,9 @@ const mount = async (app: Application) => {
   });
   server.applyMiddleware({ app, path: '/api' });
 
-  app.listen(port);
+  app.listen(process.env.PORT);
 
-  console.log(`[app]: http://localhost:${port}`);
+  console.log(`[app]: http://localhost:${process.env.PORT}`);
 }
 
 mount(express());
