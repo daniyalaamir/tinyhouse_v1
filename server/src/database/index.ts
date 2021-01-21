9dd78462faaf951ae96a1054738ff696d79a2af4
Daniyal Aamir <daamir89@gmail.com>
2021-01-21 14:57:48 -0500

    server: specify typescript generics to define the type of data to be returned from listings collection

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,12 +1,13 @@
 import { MongoClient } from 'mongodb';
+import { Database } from '../lib/types';
 
 const user = 'testuser_001';
-const userPassword = '................';
+const userPassword = 'KkuLhDSbCFWHPlVp';
 const cluster = 'cluster0.w6dvl';
 
 const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, { 
     useNewUrlParser: true,
     useUnifiedTopology: true 
