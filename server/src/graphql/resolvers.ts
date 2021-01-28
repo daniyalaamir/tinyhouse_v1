da30909ccc055bef810dffac6f037e6ef03b5166
Daniyal Aamir <daamir89@gmail.com>
2021-01-28 14:21:39 -0500

    server: modify our graphql resolvers to manipulate and use information from the database

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,20 +1,31 @@
+import { ObjectId } from 'mongodb';
 import { IResolvers } from "apollo-server-express";
-import { listings } from "../listings";
+import { Database, Listing } from '../lib/types';
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (_root: undefined, _args: {}, { db }: { db: Database }) => {
+      return await db.listings.find({}).toArray();
     }
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (
+      _root: undefined, 
+      { id }: { id: string }, 
+      { db }: { db: Database }
+    ) => {
+      const deleteResult = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id)
+      })
+
+      if(!deleteResult.value) {
+        throw new Error('failed to delete listing');
       }
-      throw new Error("failed to deleted listing");
+
+      return deleteResult.value;
     }
+  },
+  Listing: {
+    id: (listing: Listing): string => listing._id.toString()
   }
 };
